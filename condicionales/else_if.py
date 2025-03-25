58a164cc0fb32ae97c3210204d61fb4d791849b0
SrCrysis <sr.crysis2002@gmail.com>
2025-03-25 16:11:29 -0300

    new file:   condicionales/else_if.py new file:   variables/uso_variables.py new file:   variables/variables.py

---
+++ condicionales/else_if.py
@@ -0,0 +1,16 @@
+edad = 22
+
+if edad > 18:
+    print("Ud es mayor de edad")
+else:
+    print("Ud NO es mayor de edad")
+
+sueldo = 500000
+gastos = 400000
+
+if sueldo - gastos > 10000:
+    print("Ud, Tiene posbilidad de ahorrar")
+elif sueldo - gastos < 50000:
+    print("Cuidao con las moneas")
+else:
+    print("Ud, es pobre")
---
+++ variables/variables.py
@@ -0,0 +1,20 @@
+#pint(bienvenida)
+
+bienvenida = "Bienvenido"
+nombre = "Matias"
+saludo = bienvenida + " " + nombre
+
+print(saludo)
+
+numero1 = 5
+numero2 = 7
+resutlado = numero1 + numero2
+print (resutlado)
+
+final = nombre + " " + str(numero2)
+print(final)
+
+print(f"Hola {nombre}, cuantos anios tiene? {numero2}")
+
+
+print("Matias" in final)
